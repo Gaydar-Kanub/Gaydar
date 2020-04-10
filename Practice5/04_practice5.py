46571b801b4b967ccf765e3d391c5c8f6e412bf0
Gaydar-Kanub <gaydarenko@mail.ru>
2020-04-10 20:08:03 +0300

    Homework - Practice5 - 04 slide

---
+++ Practice5/04_practice5.py
@@ -0,0 +1,28 @@
+import random
+import os
+
+# way = r'C:\Users\Evgen\Desktop\Python\Practice5'
+way = os.getcwd()
+
+
+# def rand_name_002():
+#     len_name = random.randint(1, 10)
+#     name = ''
+#     for _ in range(len_name):
+#         name += chr(97 + random.randint(0, 26))
+#     return name
+#
+#
+# def pri_fd_002(way_old):
+#     for i in os.listdir(way_old):
+#         if os.path.isdir(way_old + '\\' + i):
+#             way_new = way_old + '\\' + rand_name()
+#             os.rename(way_old + '\\' + i, way_new)
+#             pri_fd(way_new)
+#     return ''
+#
+#
+# print(pri_fd_002(way))
+
+
+os.startfile(r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Paint')
