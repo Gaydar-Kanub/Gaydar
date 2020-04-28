ce274e95031a2c5eda46276f835f8444b81439b7
Gaydar-Kanub <gaydarenko@mail.ru>
2020-04-28 21:10:58 +0300

    PY110 - Lection 3

---
+++ PY110/Lection3.py
@@ -0,0 +1,23 @@
+import os
+
+path = os.getcwd()
+print(path)
+
+print(os.path.join("/tmp/1", "temp.file"))
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+# print(os.path.join(r"\tmp\1", "temp.file"))
+#
+# print(os.path.dirname(r"\tmp\1\temp.file"))
+# print(os.path.normpath(r"/tmp//2\..\1\temp.file"))
