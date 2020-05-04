8900152bccf045f436b4a96ebe271eacc9139705
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-05-04 10:04:11 +0300

    Checkio - O'Reilly - Reverse ascending

---
+++ Checkio/O'Reilly/Reverse_ascending.py
@@ -0,0 +1,28 @@
+def reverse_ascending(items):
+    if len(items) == 0:
+        return items
+    sequence = [items[0]]
+    res = []
+    for i in range(1, len(items)):
+        if items[i] > sequence[0]:
+            sequence = [items[i]] + sequence
+        else:
+            res += sequence
+            sequence = [items[i]]
+    res += sequence
+    return res
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(reverse_ascending([1, 2, 3, 4, 5]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(reverse_ascending([1, 2, 3, 4, 5])) == [5, 4, 3, 2, 1]
+    assert list(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3])) == [10, 7, 5, 4, 8, 7, 2, 3, 1]
+    assert list(reverse_ascending([5, 4, 3, 2, 1])) == [5, 4, 3, 2, 1]
+    assert list(reverse_ascending([])) == []
+    assert list(reverse_ascending([1])) == [1]
+    assert list(reverse_ascending([1, 1])) == [1, 1]
+    assert list(reverse_ascending([1, 1, 2])) == [1, 2, 1]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
