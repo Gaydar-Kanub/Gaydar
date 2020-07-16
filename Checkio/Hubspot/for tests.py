8a3ae1862e55ce9d4b8258c42d908f9bcf71a225
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-07-16 10:45:09 +0300

    new folder

---
+++ Checkio/Hubspot/for tests.py
@@ -0,0 +1,12 @@
+def test(coun, deltas):
+    if coun % deltas == 0:
+        m = coun % deltas
+    else:
+        m = coun // deltas + 1
+    d = []
+    while True:
+        i = coun
+        d.append()
+
+
+print(test(10, 3))
